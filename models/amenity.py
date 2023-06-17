6902b005b90ad6f3d3c8d19b8ce24aa88272761c
John Evwer <119025958+florastick@users.noreply.github.com>
2023-06-17 19:37:54 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""This module creates a Amenity class"""
-
+"""
+amenity module
+"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """This is a class for managing amenity objects"""
+    """
+    Amenity class
+    """
 
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Overriding constructor"""
+        super().__init__(*args, **kwargs)
