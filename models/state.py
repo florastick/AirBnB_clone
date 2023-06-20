37fdca0cb7bd720c827e80289c3d2c0e01d581f6
John Evwer <119025958+florastick@users.noreply.github.com>
2023-06-20 21:42:32 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-state module
-"""
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    State class
-    """
+    """Class for managing state objects"""
 
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Overriding constructor"""
-        super().__init__(*args, **kwargs)
