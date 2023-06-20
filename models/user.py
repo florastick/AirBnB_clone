7efb8bb0b81cd649864d708607d77da7f2134a8f
John Evwer <119025958+florastick@users.noreply.github.com>
2023-06-20 21:41:50 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-"""
-user module
-"""
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    User class
-    """
+    """Class for managing user objects"""
 
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Overriding constructor"""
-        super().__init__(*args, **kwargs)
