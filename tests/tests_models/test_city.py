d79c153c0a599fb840bc182dc342491d3837e603
John Evwer <119025958+florastick@users.noreply.github.com>
2023-06-20 21:34:59 +0100

    Update test_city.py

--- tests/tests_models/test_city.py
+++ tests/tests_models/test_city.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
-"""
-test_city module
-"""
-from unittest import TestCase
-import pycodestyle
+"""Unittest module for the City Class."""
+
+import unittest
+from datetime import datetime
+import time
 from models.city import City
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
 
 
-class TestCity(TestCase):
-    """
-    TestCity class
-    """
+class TestCity(unittest.TestCase):
 
-    def test_pep(self):
-        """test pep"""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/city.py',
-                                    'tests/test_models/test_city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    """Test Cases for the City class."""
 
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.city').__doc__
-        self.assertGreater(len(doc), 1)
+    def setUp(self):
+        """Sets up test methods."""
+        pass
 
-    def test_class_doc(self):
-        """test class documentation"""
-        doc = City.__doc__
-        self.assertGreater(len(doc), 1)
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of City class."""
+
+        b = City()
+        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
+        self.assertIsInstance(b, City)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of City class."""
+        attributes = storage.attributes()["City"]
+        o = City()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
