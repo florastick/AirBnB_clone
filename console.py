1ff9a9409f4486f02b22393a847771d278a2329e
John Evwer <119025958+florastick@users.noreply.github.com>
2023-06-20 21:16:48 +0100

    Update console.py

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """Module for the entry point of the command interpreter."""
 
 import cmd
-from models.base_model import BaseModel
+from models.base_model import base_model
 from models import storage
 import re
 import json
